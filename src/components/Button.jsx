ea8dc658b4e20c47f3b96f2acef1d715fa942a56
inevitablechange <yongho7783@gmail.com>
2024-05-10 17:52:11 +0900

    tailwind

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,19 @@
 const Button = ({ name, onClickButton }) => {
   return (
     <button
-      className="bg-gray-200 w-10 h-10 rounded-md flex justify-center items-center shadow-sm"
+      className={`bg-gray-200 w-10 h-10 rounded-md flex justify-center items-center shadow-sm hover:bg-blue-300 active:shadow-lg ${
+        name === "+"
+          ? "bg-blue-500"
+          : name === "-"
+          ? "bg-green-500"
+          : name === "*"
+          ? "bg-pink-500"
+          : name === "/"
+          ? "bg-orange-500"
+          : name === "Enter" || name === "Del"
+          ? "bg-gray-500 text-white"
+          : "bg-gray-300"
+      }`}
       onClick={onClickButton}
     >
       {name}
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,5 +1,5 @@
 const Result = ({ result }) => {
-  return <div className="bg-gray-50 p-2 rounded-md shadow-sm">{result}</div>;
+  return <div className="bg-gray-200 p-2 rounded-md shadow-sm">{result}</div>;
 };
 
 export default Result;
